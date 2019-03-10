4947437bf4ab6df7922efd63b37997ef527563f0
mykas <mykolass@wix.com>
2019-03-10 22:12:33 +0200

    <CloseButton/> - migrate to sections

---
+++ src/CloseButton/docs/examples.js
@@ -0,0 +1,25 @@
+export const skins = `
+<Layout cols={5} gap={0} justifyItems="center">
+  <CloseButton />
+  <CloseButton skin="standardFilled" />
+  <CloseButton skin="light" />
+  <CloseButton skin="lightFilled" />
+  <CloseButton skin="dark" />
+</Layout>
+`;
+
+export const custom = `
+<Layout cols={5} gap={0} justifyItems="center">
+  <CloseButton skin="standardFilled"><Help /></CloseButton>
+  <CloseButton skin="light"><Help /></CloseButton>
+  <CloseButton skin="lightFilled"><Help /></CloseButton>
+  <CloseButton skin="dark"><Help /></CloseButton>
+</Layout>
+`;
+
+export const sizes = `
+<Layout cols={2} gap={0} justifyItems="center">
+  <CloseButton />
+  <CloseButton size="medium" />
+</Layout>
+`;
--- src/CloseButton/docs/index.story.js
+++ src/CloseButton/docs/index.story.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import CloseButton from '..';
-import { storySettings } from './storySettings';
+import {
+  tab,
+  tabs,
+  api,
+  playground,
+  description,
+  divider,
+  importExample,
+  columns,
+  header,
+  title,
+  code as baseLiveCode,
+} from 'wix-storybook-utils/Sections';
 
-import CloseButtonStory from './examples';
-import Help from 'wix-ui-icons-common/Help';
+import CloseButton from '..';
+import { Layout } from '../../Layout';
+import { storySettings } from './storySettings';
+import Help from '../../new-icons/Help';
+import { baseScope } from '../../../stories/utils/LiveCodeExample';
+import testkit from '!raw-loader!./testkit.md';
+
+import * as examples from './examples';
+
+const liveCode = config =>
+  baseLiveCode({
+    previewProps: {
+      style: { backgroundColor: '#f0f4f7' },
+    },
+    compact: true,
+    components: { ...baseScope },
+    ...config,
+  });
+
+const example = ({ source, ...rest }) =>
+  columns([description({ ...rest }), liveCode({ source })]);
 
 export default {
   category: storySettings.kind,
@@ -27,5 +57,65 @@ export default {
     ],
   },
 
-  examples: <CloseButtonStory />,
+  sections: [
+    header({
+      component: (
+        <Layout gap={0}>
+          <CloseButton />
+        </Layout>
+      ),
+      sourceUrl:
+        'https://github.com/wix/wix-style-react/tree/master/src/CloseButton/CloseButton.js',
+      issueUrl: 'https://github.com/wix/wix-style-react/issues/new',
+    }),
+    tabs({
+      tabs: [
+        tab({
+          title: 'Description',
+          sections: [
+            importExample({
+              source: "import IconButton from 'wix-style-react/IconButton';",
+            }),
+
+            divider(),
+
+            columns([title('Examples')]),
+
+            ...[
+              {
+                title: 'Skins',
+                text: 'CloseButton supports 3 styles',
+                source: examples.skins,
+              },
+              {
+                title: 'Custom Icons',
+                text: 'CloseButton supports custom icons.',
+                source: examples.custom,
+              },
+              {
+                title: 'Sizes',
+                text: 'CloseButton 2 sizes',
+                source: examples.sizes,
+              },
+            ].map(example),
+          ],
+        }),
+
+        tab({
+          title: 'API',
+          sections: [api()],
+        }),
+
+        tab({
+          title: 'Testkit',
+          sections: [description({ text: testkit })],
+        }),
+
+        tab({
+          title: 'Playground',
+          sections: [playground()],
+        }),
+      ],
+    }),
+  ],
 };
