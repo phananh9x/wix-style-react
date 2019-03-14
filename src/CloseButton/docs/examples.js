d88863ca9507659f7d25857b115238eb5fb610cb
mykas <mykolass@wix.com>
2019-03-14 17:18:30 +0200

    <CloseButton/> - update styling and some code tweaks

--- src/CloseButton/docs/examples.js
+++ src/CloseButton/docs/examples.js
@@ -2,23 +2,26 @@ export const skins = `
 <Layout cols={5} gap={0} justifyItems="center">
   <CloseButton />
   <CloseButton skin="standardFilled" />
-  <CloseButton skin="light" />
-  <CloseButton skin="lightFilled" />
-  <CloseButton skin="dark" />
+  <Box backgroundColor="D10">
+    <CloseButton skin="light" />
+  </Box>
+  <Box backgroundColor="B20">
+    <CloseButton skin="lightFilled" />
+  </Box>
+  <Box backgroundColor="Y30">
+    <CloseButton skin="dark" />
+  </Box>
 </Layout>
 `;
 
 export const custom = `
 <Layout cols={5} gap={0} justifyItems="center">
-  <CloseButton skin="standardFilled"><Help /></CloseButton>
-  <CloseButton skin="light"><Help /></CloseButton>
-  <CloseButton skin="lightFilled"><Help /></CloseButton>
-  <CloseButton skin="dark"><Help /></CloseButton>
+  <CloseButton><Help /></CloseButton>
 </Layout>
 `;
 
 export const sizes = `
-<Layout cols={2} gap={0} justifyItems="center">
+<Layout cols={4} gap={0} justifyItems="center">
   <CloseButton />
   <CloseButton size="medium" />
 </Layout>
